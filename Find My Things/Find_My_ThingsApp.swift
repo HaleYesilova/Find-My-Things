495159cfb2f9ec1a38f42c628f4789f7d83e05d2
halenuryesilova <haleyesillova@gmail.com>
2025-02-18 14:11:26 +0300

    Initial Commit

---
+++ Find My Things/Find_My_ThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Find_My_ThingsApp.swift
+//  Find My Things
+//
+//  Created by Halenur Yeşilova on 18.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Find_My_ThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Find My ThingsTests/Find_My_ThingsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Find_My_ThingsTests.swift
+//  Find My ThingsTests
+//
+//  Created by Halenur Yeşilova on 18.02.2025.
+//
+
+import Testing
+@testable import Find_My_Things
+
+struct Find_My_ThingsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
